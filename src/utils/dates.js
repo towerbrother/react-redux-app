3b484a301e4c855675fc9309ff4cbdcc0dbe266f
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-01 06:58:19 +0100

    setup get request api

---
+++ src/utils/dates.js
@@ -0,0 +1,20 @@
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+export const getDate = (n = 0) =>
+  `${new Date().getFullYear() + n}-${getCurrentMonth()}-${getCurrentDay()}`;
