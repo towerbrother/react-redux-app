ed611858d567da74d926f939057267fdf996b197
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-03 16:40:08 +0100

    first display of games from store

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
       <Home />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchGames } from "../actions/gamesAction";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Game from "./Game";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -8,7 +11,40 @@ const Home = () => {
     dispatch(fetchGames());
   }, [dispatch]);
 
-  return <div>HOME</div>;
+  const { popularGames, upcomingGames, newGames } = useSelector(
+    (state) => state.games
+  );
+
+  return (
+    <StyledGameList>
+      <h2>Upcoming Games</h2>
+      <StyledGames>
+        {upcomingGames.map((game) => (
+          <Game
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+          />
+        ))}
+      </StyledGames>
+    </StyledGameList>
+  );
 };
+
+const StyledGameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const StyledGames = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
