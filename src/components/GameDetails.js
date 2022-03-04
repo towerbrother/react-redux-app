20bc3d1e78601212332c463435db7a41b608ed70
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-04 08:54:33 +0100

    completed GameDetails component

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { smallImage } from "../utils";
 import playstation from "../img/playstation.svg";
 import steam from "../img/steam.svg";
 import xbox from "../img/xbox.svg";
@@ -11,40 +13,92 @@ import gamepad from "../img/gamepad.svg";
 import starEmpty from "../img/star-empty.png";
 import starFull from "../img/star-full.png";
 
-const GameDetails = () => {
-  const { game, screenshot, isLoading } = useSelector((state) => state.details);
+const GameDetail = ({ id }) => {
+  const history = useNavigate();
+
+  const exitDetailHander = (e) => {
+    if (e.target.classList.contains("shadow")) {
+      document.body.style.overflow = "auto";
+      history.push("/");
+    }
+  };
+
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt="star" key={i} src={starFull}></img>);
+      } else {
+        stars.push(<img alt="star" key={i} src={starEmpty}></img>);
+      }
+    }
+    return stars;
+  };
+
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+
+  const { screenshot, game, isLoading } = useSelector((state) => state.details);
 
   return (
     <>
       {!isLoading && (
-        <StyledCardShadow>
-          <StyledDetails>
+        <StyledCardShadow className="shadow" onClick={exitDetailHander}>
+          <StyledDetail layoutId={id}>
             <StyledStats>
               <div className="rating">
-                <h3>{game.name}</h3>
+                <motion.h3 layoutId={`title ${id}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
+                {getStars()}
               </div>
               <StyledInfo>
                 <h3>Platforms</h3>
                 <StyledPlatforms>
-                  {game.platforms.map((item) => (
-                    <h3 key={item.platform.id}>{item.platform.name}</h3>
+                  {game.platforms.map((data) => (
+                    <img
+                      alt={data.platform.name}
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                    ></img>
                   ))}
                 </StyledPlatforms>
               </StyledInfo>
             </StyledStats>
             <StyledMedia>
-              <img src={game.background_image} alt={game.name} />
+              <motion.img
+                layoutId={`image ${id}`}
+                src={smallImage(game.background_image, 1280)}
+                alt={game.background_image}
+              />
             </StyledMedia>
             <StyledDescription>
               <p>{game.description_raw}</p>
             </StyledDescription>
             <div className="gallery">
-              {screenshot.results.map((item) => (
-                <img key={item.id} src={item.image} alt="game" />
+              {screenshot.results.map((screenshot) => (
+                <img
+                  src={smallImage(screenshot.image, 1280)}
+                  key={screenshot.id}
+                  alt={screenshot.image}
+                />
               ))}
             </div>
-          </StyledDetails>
+          </StyledDetail>
         </StyledCardShadow>
       )}
     </>
@@ -71,7 +125,7 @@ const StyledCardShadow = styled(motion.div)`
   }
 `;
 
-const StyledDetails = styled(motion.div)`
+const StyledDetail = styled(motion.div)`
   width: 80%;
   border-radius: 1rem;
   padding: 2rem 5rem;
@@ -117,4 +171,4 @@ const StyledDescription = styled(motion.div)`
   margin: 5rem 0rem;
 `;
 
-export default GameDetails;
+export default GameDetail;
